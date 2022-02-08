7c7cf3b76b5ea30b065a8b852b1efe3ca5c26e81
cabicho <jcabicho@gmail.com>
2022-02-08 21:05:19 +0000

    add schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
 
+import {MongooseModule} from '@nestjs/mongoose'
 @Module({
-  imports: [TweetsModule],
+  imports: [
+      MongooseModule.forRoot(
+          'mongodb://root:root@db/analytics?authSource=admin', //mongodb e' o protocolo// usuario:senha
+        { 
+          useNewUrlParser: true
+        }
+      ),
+      TweetsModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
